6af9c998408bf8fc00baf678dac2393755f47a72
zhoukaiwen <850148975@qq.com>
2020-11-12 00:04:40 +0800

    first_commit

---
+++ study-springcloud/eureka-server/src/test/java/com/zhoukaiwen/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhoukaiwen.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
